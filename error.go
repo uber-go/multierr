6c176c375e9f56fffc4ca51d3c300965e204a775
Abhinav Gupta <abg@uber.com>
2017-03-22 16:43:46 -0700

    Use vanity import

--- error.go
+++ error.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package multierr
+package multierr // import "go.uber.org/multierr"
 
 import (
 	"bytes"
