b31efe560dc5b6617125e2bde29f5eed9a617c4f
Abhinav Gupta <abg@uber.com>
2020-10-26 17:04:58 -0700

    Drop Go 1.12 support
    Support for `errors.Is` and `errors.As` was special-cased to Go 1.13 or
    higher. This is no longer necessary since, as of right now, only Go 1.14
    and 1.15 need to be supported.

--- error_ext_test.go
+++ error_ext_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Uber Technologies, Inc.
+// Copyright (c) 2020 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -17,8 +17,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
-// +build go1.13
 
 package multierr_test
 
