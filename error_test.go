445999037311d4b33da9ece1730660e666862821
Alexandr Burdiyan <burdiyan@gmail.com>
2022-09-06 18:20:06 +0200

    Add a shorthand for AppendInvoke (#65)
    I really like the idea of catching returned errors from deferred functions. Though having to use `multierr` package name twice in the same line makes it a bit verbose in many occasions.
    
    This PR introduces a shorthand for AppendInvoke which allows passing function or method value directly without wrapping it into an Invoker.
    
    So this:
    
    ```go
    defer multierr.AppendInvoke(&err, multierr.Invoke(my.StopFunc))
    ```
    
    could become this:
    
    ```go
    defer multierr.AppendFunc(&err, my.StopFunc)
    ```
    
    Co-authored-by: Sung Yoon Whang <sungyoonwhang@gmail.com>

--- error_test.go
+++ error_test.go
@@ -675,6 +675,24 @@ func TestAppendIntoNil(t *testing.T) {
 	})
 }
 
+func TestAppendFunc(t *testing.T) {
+	var (
+		errDeferred = errors.New("deferred func called")
+		errOriginal = errors.New("original error")
+	)
+
+	stopFunc := func() error {
+		return errDeferred
+	}
+
+	err := func() (err error) {
+		defer AppendFunc(&err, stopFunc)
+
+		return errOriginal
+	}()
+	assert.Equal(t, []error{errOriginal, errDeferred}, Errors(err), "both deferred and original error must be returned")
+}
+
 func errorPtr(err error) *error {
 	return &err
 }
